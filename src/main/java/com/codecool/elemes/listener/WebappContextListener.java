d92d0d983860f2b25e6dd52859e693015af0236d
norcs <norbertcs@me.com>
2018-03-19 19:30:35 +0100

    Add Database instance creation to WebappContextListener and character  encoding filter

---
+++ src/main/java/com/codecool/elemes/listener/WebappContextListener.java
@@ -0,0 +1,28 @@
+package com.codecool.elemes.listener;
+
+import com.codecool.elemes.model.Database;
+import com.codecool.elemes.model.FileHandle;
+import com.codecool.elemes.model.Role;
+import com.codecool.elemes.model.User;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public final class WebappContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        Database database = Database.createInstance();
+        ServletContext ctx = sce.getServletContext();
+        sce.getServletContext().addFilter("SetCharacterEncodingFilter", "org.apache.catalina.filters.SetCharacterEncodingFilter");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("Server shutting down...");
+
+    }
+}
