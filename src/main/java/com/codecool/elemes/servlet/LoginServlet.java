fde5dff46cf25f225ba5641fbe4fd6e9538f9948
norcs <norbertcs@me.com>
2018-03-20 00:04:03 +0100

    Add LoginService and implement doPost
    User can login after checking if he/she already registered user or not

---
+++ src/main/java/com/codecool/elemes/servlet/LoginServlet.java
@@ -0,0 +1,29 @@
+package com.codecool.elemes.servlet;
+
+import com.codecool.elemes.service.LoginService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    private final LoginService loginService = new LoginService();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (loginService.isRegistered(req)) {
+            req.setAttribute("user", req.getParameter("user"));
+            resp.sendRedirect("homepage");
+        } else {
+            req.setAttribute("message", "Invalid login data, please check.");
+            req.getRequestDispatcher("index").include(req, resp);
+        }
+    }
+}
+
