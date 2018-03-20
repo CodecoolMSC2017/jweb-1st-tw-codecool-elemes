be1baf7b6a127924f2036adcd99b77ee99a1c5ae
gaboroneSzabo <puma678@gmail.com>
2018-03-20 20:24:41 +0100

    Refactor.

--- src/main/java/com/codecool/elemes/servlet/PagesServlet.java
+++ src/main/java/com/codecool/elemes/servlet/PagesServlet.java
@@ -1,8 +1,5 @@
 package com.codecool.elemes.servlet;
 
-import com.codecool.elemes.model.Database;
-import com.codecool.elemes.model.Role;
-import com.codecool.elemes.model.Text;
 import com.codecool.elemes.model.User;
 import com.codecool.elemes.service.PageService;
 
@@ -13,12 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 @WebServlet("/pages")
 public class PagesServlet extends HttpServlet {
-    PageService pageService = new PageService();
+    private PageService pageService = new PageService();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
