bc8f699dd04d67a9ba722bf4e9adff8815c9e1d6
norcs <norbertcs@me.com>
2018-03-20 19:41:48 +0100

    Add session

--- src/main/java/com/codecool/elemes/servlet/LoginServlet.java
+++ src/main/java/com/codecool/elemes/servlet/LoginServlet.java
@@ -1,5 +1,8 @@
 package com.codecool.elemes.servlet;
 
+import com.codecool.elemes.exceptions.NoSuchUserException;
+import com.codecool.elemes.model.Database;
+import com.codecool.elemes.model.User;
 import com.codecool.elemes.service.LoginService;
 
 import javax.servlet.ServletException;
@@ -7,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 
@@ -16,19 +20,25 @@ public class LoginServlet extends HttpServlet {
     private final LoginService loginService = new LoginService();
 
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if (loginService.isRegistered(req)) {
-            req.setAttribute("user", req.getParameter("user"));
-            resp.sendRedirect("homepage.jsp");
-        } else {
-            req.setAttribute("message", "Invalid login data, please check.");
-            req.getRequestDispatcher("/index.jsp").include(req, resp);
-            try{
-                req.getRequestDispatcher("/footer.jsp").include(req, resp);
-            }catch (ServletException e){
+            try {
+                User user = Database.getInstance().getUser(req.getParameter("email"));
+                HttpSession session = req.getSession();
+                session.setAttribute("loggedin", user );
+                resp.sendRedirect("userpage");
+            } catch (NoSuchUserException e) {
                 e.printStackTrace();
             }
 
+        } else {
+            req.setAttribute("message", "Invalid login data, please check.");
+            req.getRequestDispatcher("/index.jsp").include(req, resp);
         }
     }
 }
