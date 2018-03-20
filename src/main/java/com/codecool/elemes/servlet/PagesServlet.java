0210bec574dab8deab360796e7a92c6fbb2ec03e
gaboroneSzabo <puma678@gmail.com>
2018-03-20 20:23:39 +0100

    Get user from session, refactor.

--- src/main/java/com/codecool/elemes/servlet/PagesServlet.java
+++ src/main/java/com/codecool/elemes/servlet/PagesServlet.java
@@ -4,35 +4,27 @@ import com.codecool.elemes.model.Database;
 import com.codecool.elemes.model.Role;
 import com.codecool.elemes.model.Text;
 import com.codecool.elemes.model.User;
+import com.codecool.elemes.service.PageService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/pages")
 public class PagesServlet extends HttpServlet {
+    PageService pageService = new PageService();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = new User("a","a", Role.MENTOR);
-        Database database = Database.getInstance();
-        List<Text> texts = new ArrayList<>();
-        if (user.getRole().equals(Role.STUDENT)) {
-            for (Text text : database.getTexts()) {
-                if (text.isPublished()) {
-                    texts.add(text);
-                }
-            }
+        HttpSession session = req.getSession();
+        User user = (User)session.getAttribute("loggedin");
 
-        }
-        else{
-            texts = database.getTexts();
-        }
-        req.setAttribute("texts", texts);
+        req.setAttribute("texts",pageService.getTexts(user));
         req.getRequestDispatcher("pages.jsp").forward(req, resp);
 
     }
