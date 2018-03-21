7d50379b419226839c194f328a16309a2382428e
gaboroneSzabo <puma678@gmail.com>
2018-03-21 14:52:48 +0100

    Add mentor text page update form and servlet implementation.

---
+++ src/main/java/com/codecool/elemes/service/ContentHandler.java
@@ -0,0 +1,19 @@
+package com.codecool.elemes.service;
+
+import com.codecool.elemes.exceptions.InvalidInputException;
+import com.codecool.elemes.model.Database;
+import com.codecool.elemes.model.Text;
+import com.codecool.elemes.model.TextDatabase;
+
+public class ContentHandler {
+    private TextDatabase database = Database.getInstance();
+
+    public void addNewTextContent(String title, String content) throws InvalidInputException {
+        if (title == "" || content == "") {
+            throw new InvalidInputException();
+        }
+        Text text = new Text(title, false);
+        text.setContent(content);
+        database.addText(text);
+    }
+}
---
+++ src/main/java/com/codecool/elemes/servlet/AddContent.java
@@ -0,0 +1,31 @@
+package com.codecool.elemes.servlet;
+
+import com.codecool.elemes.exceptions.InvalidInputException;
+import com.codecool.elemes.service.ContentHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/addcontent")
+public class AddContent extends HttpServlet {
+
+    ContentHandler contentHandler = new ContentHandler();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String title = req.getParameter("title");
+        String content = req.getParameter("content");
+        try {
+            contentHandler.addNewTextContent(title, content);
+        } catch (InvalidInputException e) {
+            req.setAttribute("message", "Invalid input");
+            req.getRequestDispatcher("pages").include(req, resp);
+        }
+        req.setAttribute("message", "Text added");
+        req.getRequestDispatcher("pages").include(req, resp);
+    }
+}
