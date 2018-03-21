328a45d5792f4c555dba6a492c3bbd7f9452f5b1
gaboroneSzabo <puma678@gmail.com>
2018-03-21 17:54:58 +0100

    Add new field: id, add default content text.

--- src/main/java/com/codecool/elemes/model/Text.java
+++ src/main/java/com/codecool/elemes/model/Text.java
@@ -1,13 +1,19 @@
 package com.codecool.elemes.model;
 
+import java.util.Random;
+
 public class Text {
     private String text;
     private boolean isPublished;
     private String content;
+    private int id;
 
     public Text(String text, boolean isPublished) {
         this.text = text;
         this.isPublished = isPublished;
+        Random random = new Random();
+        id = random.nextInt(1000000);
+        content = "default content";
     }
 
     public void publish() {
@@ -36,5 +42,9 @@ public class Text {
 
     public String getContent() {
         return content;
+    }
+
+    public int getId() {
+        return id;
     }
 }
