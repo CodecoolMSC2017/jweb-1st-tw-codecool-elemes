c8786495ed4f8b452fad75bc1799091f8db2b20e
norcs <norbertcs@me.com>
2018-03-21 19:38:48 +0100

    Rename link, add code to check if an email is already taken

--- src/main/java/com/codecool/elemes/servlet/RegisterServlet.java
+++ src/main/java/com/codecool/elemes/servlet/RegisterServlet.java
@@ -3,30 +3,40 @@ package com.codecool.elemes.servlet;
 import com.codecool.elemes.model.Database;
 import com.codecool.elemes.model.Role;
 import com.codecool.elemes.model.User;
-
+import com.codecool.elemes.service.LoginService;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet("/registration")
+@WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
+
+    private final LoginService loginService = new LoginService();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("registration.jsp").forward(req, resp);
+        req.getRequestDispatcher("register.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = req.getParameter("name");
-        String email = req.getParameter("email");
-        String role = req.getParameter("role");
-        User user = new User(name, email, Role.valueOf(role));
-        Database.getInstance().add(user);
-        req.setAttribute("user", user);
-        resp.sendRedirect("userpage.jsp");
+        if(!loginService.isRegistered(req)) {
+            String name = req.getParameter("name");
+            String email = req.getParameter("email");
+            String role = req.getParameter("role");
+            User user = new User(name, email, Role.valueOf(role));
+            Database.getInstance().add(user);
+            HttpSession session = req.getSession();
+            session.setAttribute("loggedin", user );
+            req.setAttribute("user", user);
+            resp.sendRedirect("userpage");
+        } else {
+            req.setAttribute("error", "The email address you have entered is already registered");
+            req.getRequestDispatcher("register.jsp").include(req, resp);
+        }
     }
 }
