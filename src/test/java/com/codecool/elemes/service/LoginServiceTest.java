7a9ff76e5186c82053a8f459c5a03d0aaec055b8
gabor <kovacsgaboo@gmail.com>
2018-04-03 13:52:44 +0200

    creating test and started working on it

---
+++ src/test/java/com/codecool/elemes/service/LoginServiceTest.java
@@ -0,0 +1,37 @@
+package com.codecool.elemes.service;
+
+import com.codecool.elemes.exceptions.NoSuchUserException;
+import com.codecool.elemes.model.Database;
+import com.codecool.elemes.model.Role;
+import com.codecool.elemes.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginServiceTest {
+    Database database = new Database();
+
+
+    @BeforeEach
+    void setUp() {
+        database.add(new User("peter", "peter@gamil.fr", Role.STUDENT));
+    }
+
+    @Test
+    void isRegistered() {
+        //given
+        String email1 = "peter@gamil.fr";
+        String email2 = "asdasd@gmail.com";
+
+
+        assertThrows(NoSuchUserException.class, () -> {
+            database.getUser("peter@gamil.fr").geteMail();
+        });
+    }
+
+    @Test
+    void createUser() {
+
+    }
+}
