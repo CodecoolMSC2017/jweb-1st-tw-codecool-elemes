45e8bcb3126a7e5c61461843f34397f5057c157f
Kenezke1 <herczeg.kenya2@gmail.com>
2018-04-16 14:56:23 +0200

    Implement UserDao,AbstractDao

--- src/main/java/com/codecool/elemes/dao/UserDataBase.java
+++ src/main/java/com/codecool/elemes/dao/UserDataBase.java
@@ -3,15 +3,17 @@ package com.codecool.elemes.dao;
 import com.codecool.elemes.exceptions.NoSuchUserException;
 import com.codecool.elemes.model.User;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public interface UserDataBase {
 
-    void add(User user);
+    void add(User user) throws SQLException;
 
-    List<User> getAllUser();
+    List<User> getAllUser() throws SQLException;
 
-    User getUser(String email) throws NoSuchUserException;
+    User getUser(String email) throws NoSuchUserException, SQLException;
 
-    List<User> getOnlyStudents(List<User> users);
+    List<User> getOnlyStudents(List<User> users) throws SQLException;
 }
