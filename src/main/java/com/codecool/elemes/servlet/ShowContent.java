13acf5f6e861e15510b04acc27a570629f8bcfed
gabor <kovacsgaboo@gmail.com>
2018-04-16 15:01:26 +0200

    refactor to work  sql database

--- src/main/java/com/codecool/elemes/servlet/ShowContent.java
+++ src/main/java/com/codecool/elemes/servlet/ShowContent.java
@@ -1,5 +1,6 @@
 package com.codecool.elemes.servlet;
 
+import com.codecool.elemes.dao.TextDao;
 import com.codecool.elemes.service.ContentShowService;
 
 import javax.servlet.ServletException;
@@ -9,19 +10,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/content")
-public class ShowContent extends HttpServlet {
-    private ContentShowService contentShowService = new ContentShowService();
+public class ShowContent extends AbstractServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int id = Integer.parseInt(req.getParameter("id"));
-        String content = contentShowService.getContent(id);
-        String[] asd;
-        asd = content.split("\n");
-        req.setAttribute("content",asd);
-        req.getRequestDispatcher("content.jsp").forward(req, resp);
+        try (Connection connection = getConnection(req.getServletContext())){
+            TextDao textDao = new TextDao(connection);
+            ContentShowService contentShowService = new ContentShowService(textDao);
+            String content = contentShowService.getContent(id);
+
+            String[] asd;
+            asd = content.split("\n");
+            req.setAttribute("content",asd);
+            req.getRequestDispatcher("content.jsp").forward(req, resp);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
