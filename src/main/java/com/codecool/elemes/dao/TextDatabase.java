8d40a3afc06ba1551d19fd15a122ee5771f1cb60
gabor <kovacsgaboo@gmail.com>
2018-04-16 14:54:50 +0200

    add exceptions

--- src/main/java/com/codecool/elemes/dao/TextDatabase.java
+++ src/main/java/com/codecool/elemes/dao/TextDatabase.java
@@ -4,15 +4,16 @@ import com.codecool.elemes.exceptions.NoSuchTextException;
 import com.codecool.elemes.exceptions.TextNotFoundException;
 import com.codecool.elemes.model.Text;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface TextDatabase {
 
-    List<Text> getTexts();
+    List<Text> getTexts() throws SQLException;
 
-    void addText(Text text);
+    void addText(Text text) throws SQLException;
 
     void deleteText(Text text) throws NoSuchTextException;
 
-    Text getText(int id) throws TextNotFoundException;
+    Text getText(int id) throws TextNotFoundException, SQLException;
 }
