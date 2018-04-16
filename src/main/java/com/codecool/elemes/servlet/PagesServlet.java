de5a1cd3518989037f7cf36b1ac4accad289499c
gabor <kovacsgaboo@gmail.com>
2018-04-16 15:03:43 +0200

    rework to work with sql database

--- src/main/java/com/codecool/elemes/servlet/PagesServlet.java
+++ src/main/java/com/codecool/elemes/servlet/PagesServlet.java
@@ -1,5 +1,6 @@
 package com.codecool.elemes.servlet;
 
+import com.codecool.elemes.dao.TextDao;
 import com.codecool.elemes.model.User;
 import com.codecool.elemes.service.PageService;
 
@@ -10,28 +11,45 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet("/pages")
-public class PagesServlet extends HttpServlet {
-    private PageService pageService = new PageService();
+public class PagesServlet extends AbstractServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        User user = (User)session.getAttribute("loggedin");
+        User user = (User) session.getAttribute("loggedin");
+        try (Connection connection = getConnection(req.getServletContext())) {
+            TextDao textDao = new TextDao(connection);
+            PageService pageService = new PageService(textDao);
 
-        req.setAttribute("texts",pageService.getTexts(user));
-        req.getRequestDispatcher(pageService.getPage(user)).forward(req, resp);
+            req.setAttribute("texts", pageService.getTexts(user));
+            req.getRequestDispatcher(pageService.getPage(user)).forward(req, resp);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        User user = (User)session.getAttribute("loggedin");
+        User user = (User) session.getAttribute("loggedin");
+        try (Connection connection = getConnection(req.getServletContext())) {
+            TextDao textDao = new TextDao(connection);
+            PageService pageService = new PageService(textDao);
 
-        pageService.handlePublish(req);
+            pageService.handlePublish(req);
 
-        req.setAttribute("texts",pageService.getTexts(user));
-        req.getRequestDispatcher(pageService.getPage(user)).forward(req, resp);
+            req.setAttribute("texts", pageService.getTexts(user));
+            req.getRequestDispatcher(pageService.getPage(user)).forward(req, resp);
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
