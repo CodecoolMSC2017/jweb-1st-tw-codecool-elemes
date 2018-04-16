063e88dd3cd83f5381141a628f62c2b8597cad9f
Kenezke1 <herczeg.kenya2@gmail.com>
2018-04-16 13:54:53 +0200

    Create Abstract dao class

---
+++ src/main/java/com/codecool/elemes/dao/AbstractDao.java
@@ -0,0 +1,15 @@
+package com.codecool.elemes.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+abstract class AbstractDao {
+
+    final Connection connection;
+
+    AbstractDao(Connection connection) {
+        this.connection = connection;
+    }
+}
