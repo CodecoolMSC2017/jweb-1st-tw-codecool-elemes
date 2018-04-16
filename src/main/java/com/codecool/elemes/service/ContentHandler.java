73a9ceafc6132c6e7855c6b505ddbef4d91277b4
gabor <kovacsgaboo@gmail.com>
2018-04-16 15:02:41 +0200

    refactor the constructor

--- src/main/java/com/codecool/elemes/service/ContentHandler.java
+++ src/main/java/com/codecool/elemes/service/ContentHandler.java
@@ -1,19 +1,25 @@
 package com.codecool.elemes.service;
 
+import com.codecool.elemes.dao.TextDao;
 import com.codecool.elemes.exceptions.InvalidInputException;
 import com.codecool.elemes.model.Database;
 import com.codecool.elemes.model.Text;
 import com.codecool.elemes.dao.TextDatabase;
 
-public class ContentHandler {
-    private TextDatabase database = Database.getInstance();
+import java.sql.SQLException;
 
-    public void addNewTextContent(String title, String content) throws InvalidInputException {
+public class ContentHandler {
+    private TextDatabase database ;
+
+    public ContentHandler(TextDatabase database) {
+        this.database = database;
+    }
+
+    public void addNewTextContent(String title, String content) throws InvalidInputException, SQLException {
         if (title.equals("") || content.equals("")) {
             throw new InvalidInputException();
         }
-        Text text = new Text(title, false);
-        text.setContent(content);
+        Text text = new Text(title, false,content);
         database.addText(text);
     }
 }
