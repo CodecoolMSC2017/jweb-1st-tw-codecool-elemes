0961ab77756f34e3fb99431b0e078cb2daba5814
gabor <kovacsgaboo@gmail.com>
2018-04-16 15:03:09 +0200

    refactor the constructor

--- src/main/java/com/codecool/elemes/service/ContentShowService.java
+++ src/main/java/com/codecool/elemes/service/ContentShowService.java
@@ -5,11 +5,17 @@ import com.codecool.elemes.model.Database;
 import com.codecool.elemes.model.Text;
 import com.codecool.elemes.dao.TextDatabase;
 
+import java.sql.SQLException;
+
 public final class ContentShowService {
 
-    TextDatabase database = Database.getInstance();
+    TextDatabase database ;
 
-    public String getContent(int textId) {
+    public ContentShowService(TextDatabase database) {
+        this.database = database;
+    }
+
+    public String getContent(int textId) throws SQLException {
         Text text;
         try {
             text = database.getText(textId);
